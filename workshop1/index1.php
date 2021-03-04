8bd214e3ff5ceaffe2642697e5604a43f5dd5274
mohammed <mohammedkabli53@gmail.com>
2021-03-04 22:30:13 +0100

    workshop1

---
+++ workshop1/index1.php
@@ -0,0 +1,20 @@
+ 
+ <!--step1-->
+ <?php
+echo ("bonjour le monde !<br>");
+echo 'Today is :'. date('d/m/y h:i:sa');
+?>
+
+<!--step2-->
+
+<?php
+$a       = "Hello ";
+$b       = 2021;
+$c   = "Year";
+
+echo ("<h2>$a $b $c !</h2><br>");
+
+
+?>
+
+
